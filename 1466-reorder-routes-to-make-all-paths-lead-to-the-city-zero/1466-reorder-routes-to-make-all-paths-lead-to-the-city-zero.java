ff2d09e3d87296f7f828db5aeaf211d535d31f0f
Adrish karak <147429771+AdrishKarak@users.noreply.github.com>
2026-08-24 23:40:30 +0530

    Time: 80 ms (21.54%), Space: 131.7 MB (22.14%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,34 @@
+class Solution {
+    int ans=0;
+    private void dfs(int node, List<Integer>[] adj, List<Integer>[] frm, boolean[] vis){
+        vis[node] = true;
+        for(int nei : adj[node]){
+            if(vis[nei]) continue;
+            if(frm[node].contains(nei)) 
+            ans++;
+            dfs(nei, adj, frm, vis);
+        }
+    }
+
+    public int minReorder(int n, int[][] connections) {
+        List<Integer>[] adj = new ArrayList[n];
+        List<Integer>[] frm = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+            frm[i] = new ArrayList<>();
+        }
+        for (int[] con : connections) {
+            int u = con[0];
+            int v = con[1];
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+        for (int[] con : connections) {
+            frm[con[0]].add(con[1]);
+        }
+        dfs(0, adj, frm, new boolean[n]);
+        return ans;
+    }
+
+
+}
