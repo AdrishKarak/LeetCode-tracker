f254487bd221fa54837f79dbb6c397391201088b
Adrish karak <147429771+AdrishKarak@users.noreply.github.com>
2026-08-27 20:43:20 +0530

    Time: 10 ms (40.53%), Space: 104.6 MB (66.33%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,18 +1,27 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        // indegree[i] = number of incoming edges to node i
         int[] indegree = new int[n];
 
-        for(List<Integer> edge : edges){
+        // Count incoming edges for every node
+        for (List<Integer> edge : edges) {
+
             int to = edge.get(1);
+
             indegree[to]++;
         }
 
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            if(indegree[i]==0){
-                ans.add(i);
+        List<Integer> answer = new ArrayList<>();
+
+        // Nodes with indegree 0 cannot be reached
+        // from any other node, so they must be included
+        for (int node = 0; node < n; node++) {
+
+            if (indegree[node] == 0) {
+                answer.add(node);
             }
         }
-        return ans;
+
+        return answer;
     }
 }
