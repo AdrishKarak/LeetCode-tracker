b064aabaac657254f1f111c583819714f4fdea4e
Adrish karak <147429771+AdrishKarak@users.noreply.github.com>
2026-09-22 01:16:36 +0530

    Time: 2 ms (91.68%), Space: 48.2 MB (6.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        //initialize a variable left to keep track of the position where the next non-zero element will be moved
         int left =0;
 
         for(int right=0; right<nums.length; right++){
