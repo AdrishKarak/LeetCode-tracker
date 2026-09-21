1e6e7912bba96bc327333ab47367063302bd9d7a
Adrish karak <147429771+AdrishKarak@users.noreply.github.com>
2026-09-22 01:37:40 +0530

    Time: 0 ms (100%), Space: 47.5 MB (22.54%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total=0;
+        for(int num : nums){
+            total += num;
+        }
+
+
+        int ltotal=0;
+        for(int i=0; i<nums.length; i++){
+            int rtotal = total - ltotal - nums[i];
+            if(rtotal == ltotal){
+                return i;
+            }
+            ltotal += nums[i];
+        }
+        return -1;
+    }
+}
