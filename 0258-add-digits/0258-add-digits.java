bff4d2cb63f97fdfd63c7a7613087ac95e429017
Adrish karak <147429771+AdrishKarak@users.noreply.github.com>
2026-09-26 14:46:31 +0530

    nm

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -12,3 +12,20 @@ class Solution {
         return num;
     }
 }
+
+//Math Approach:
+//Zero case: If num is 0, digital root is 0
+//Multiple of 9: If num % 9 equals 0 (and num ≠ 0), digital root is 9
+//General case: For all other numbers, digital root is num % 9
+//Mathematical property: This works because repeatedly summing digits is equivalent to finding the remainder when divided by 9
+//Constant time: No loops or recursion needed
+
+//class Solution {
+//public int addDigits(int num) {
+//       if (num == 0)
+//       return 0;
+//       if (num % 9 == 0)
+//       return 9;
+//       return num % 9;
+//        }
+//     }
